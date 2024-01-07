91c15bc9dde2de57e0dfa2e0919c8c43654f474c
shahid chaudhary <shahidchaudhary583@gmail.com>
2024-01-07 10:28:50 +0530

    first commit

---
+++ constructor-fun/c.js
@@ -0,0 +1,11 @@
+// normal function me jb this keyword ka use kiya jaye or fun ko call krte time new keyword ka istemal kiya jjaye then it will be constructor function 
+function shaid(){
+    this.width = 12;
+    this.height = 22;
+    this.color = "brown";
+    this.taste = "salty";
+}
+let bisk1= new shaid();
+let bisk2= new  shaid();
+let bisk3= new shaid();
+let bisk4= new  shaid();
---
+++ higher-oder-fun/h.js
@@ -0,0 +1,3 @@
+// esa  function jo accept krle ek or function or esa function jo return krde function is called higher order fun
+
+// function(){function(){}} *******
